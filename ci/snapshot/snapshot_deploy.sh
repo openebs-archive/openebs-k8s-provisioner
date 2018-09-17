de643369d8c97bdc3f4a20a543465290dd8a9d83
prateekpandey14 <prateekpandey14@gmail.com>
2018-09-17 18:23:52 +0530

    fix(CI): Run clone integration test in travis CI
     Run integration test for clone volume creation as part of CI
     run which was commented out in earlier PR #66 due to its flaky
     behaviour
    
    Signed-off-by: prateekpandey14 <prateekpandey14@gmail.com>

--- ci/snapshot/snapshot_deploy.sh
+++ ci/snapshot/snapshot_deploy.sh
@@ -73,48 +73,48 @@ kubectl logs --tail=20 -n openebs deployment/openebs-snapshot-operator -c snapsh
 
 sleep 30
 # get clone replica pod IP to make a curl request to get hte clone status
-cloned_replica_ip=$(kubectl get pods -owide -l pvc=demo-snap-vol-claim --no-headers | grep -v ctrl | awk {'print $6'})
+cloned_replica_ip=$(kubectl get pods -owide -l openebs.io/persistent-volume-claim=demo-snap-vol-claim --no-headers | grep -v ctrl | awk {'print $6'})
 echo "***************** checking clone status *********************************"
-for i in $(seq 1 100) ; do
+for i in $(seq 1 5) ; do
 		clonestatus=`curl http://$cloned_replica_ip:9502/v1/replicas/1 | jq '.clonestatus' | tr -d '"'`
 		if [ "$clonestatus" == "completed" ]; then
         break
 			else
         echo "Clone process in not completed ${clonestatus}"
-        sleep 10
+        sleep 60
     fi
 done
 
 # Clone is in Alpha state, and kind of flaky sometimes, comment this integration test below for time being,
 # util its stable in backend storage engine
-#sleep 30
-#echo "***************Creating busybox-clone application pod********************"
-#kubectl create -f $DST_REPO/external-storage/openebs/ci/snapshot/busybox_clone.yaml
-#
-#kubectl get pods --all-namespaces
-#kubectl get pvc --all-namespaces
-#
-#for i in $(seq 1 50) ; do
-#    phase=$(kubectl get pods busybox-clone --output="jsonpath={.status.phase}")
-#    if [ "$phase" == "Running" ]; then
-#        break
-#    else
-#        echo "--------------busybox-clone pod is not ready yet-----------------"
-#        kubectl describe pods busybox-clone
-#    sleep 10
-#    fi
-#done
-#
-#kubectl get pods
+sleep 30
+echo "***************Creating busybox-clone application pod********************"
+kubectl create -f $DST_REPO/external-storage/openebs/ci/snapshot/busybox_clone.yaml
+
+kubectl get pods --all-namespaces
+kubectl get pvc --all-namespaces
+
+for i in $(seq 1 50) ; do
+    phase=$(kubectl get pods busybox-clone --output="jsonpath={.status.phase}")
+    if [ "$phase" == "Running" ]; then
+        break
+    else
+        echo "--------------busybox-clone pod is not ready yet-----------------"
+        kubectl describe pods busybox-clone
+		sleep 10
+    fi
+done
+
+kubectl get pods
 kubectl get pvc
-#
-#echo "*************Varifying data validity and Md5Sum Check********************"
-#hash1=$(kubectl exec busybox -- md5sum /mnt/store1/date.txt | awk '{print $1}')
-#hash2=$(kubectl exec busybox-clone -- md5sum /mnt/store2/date.txt | awk '{print $1}')
-#echo "busybox hash: $hash1"
-#echo "busybox-clone hash: $hash2"
-#if [[ $hash1 == $hash2 ]]; then
-#	 echo "Md5Sum Check: PASSED"
-#else
-#echo "Md5Sum Check: FAILED"; exit 1
-#fi
+
+echo "*************Varifying data validity and Md5Sum Check********************"
+hash1=$(kubectl exec busybox -- md5sum /mnt/store1/date.txt | awk '{print $1}')
+hash2=$(kubectl exec busybox-clone -- md5sum /mnt/store2/date.txt | awk '{print $1}')
+echo "busybox hash: $hash1"
+echo "busybox-clone hash: $hash2"
+if [[ $hash1 == $hash2 ]]; then
+	 echo "Md5Sum Check: PASSED"
+else
+echo "Md5Sum Check: FAILED"; exit 1
+fi
