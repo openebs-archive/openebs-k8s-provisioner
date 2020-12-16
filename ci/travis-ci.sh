f18a8e3a1f33b5f5d8dc6c6a41680021419287c4
kmova <kiran.mova@mayadata.io>
2020-12-16 20:22:11 +0000

    fix(travis): revert main branch on openebs to master
    Signed-off-by: kmova <kiran.mova@mayadata.io>

--- ci/travis-ci.sh
+++ ci/travis-ci.sh
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 # global env vars to be used in test scripts
-export CI_BRANCH="develop"
+export CI_BRANCH="master"
 export CI_TAG="ci"
 
 #./ci/helm_install_openebs.sh
