2b0f6a4d9bf371cdedffd16cb6b64201007cd4fa
prateekpandey14 <prateekpandey14@gmail.com>
2021-08-21 15:41:21 +0530

    fix(crds): update snapshot CRDs version to use v1 apis
    Signed-off-by: prateekpandey14 <prateekpandey14@gmail.com>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -23,7 +23,7 @@ import (
 
 	"github.com/golang/glog"
 	crdv1 "github.com/openebs/openebs-k8s-provisioner/pkg/apis/crd/v1"
-	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -31,6 +31,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/rest"
+	utilpointer "k8s.io/utils/pointer"
 )
 
 const (
@@ -62,52 +63,77 @@ func NewClient(cfg *rest.Config) (*rest.RESTClient, *runtime.Scheme, error) {
 
 // CreateCRD creates CustomResourceDefinition
 func CreateCRD(clientset apiextensionsclient.Interface) error {
-	crd := &apiextensionsv1beta1.CustomResourceDefinition{
+	crd := &apiextensionsv1.CustomResourceDefinition{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: crdv1.VolumeSnapshotDataResourcePlural + "." + crdv1.GroupName,
 			Annotations: map[string]string{
 				"api-approved.kubernetes.io": "https://github.com/kubernetes-csi/external-snapshotter/pull/419",
 			},
 		},
-		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-			Group:   crdv1.GroupName,
-			Version: crdv1.SchemeGroupVersion.Version,
-			Scope:   apiextensionsv1beta1.ClusterScoped,
-			Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
+		Spec: apiextensionsv1.CustomResourceDefinitionSpec{
+			Group: crdv1.GroupName,
+			Versions: []apiextensionsv1.CustomResourceDefinitionVersion{
+				{
+					Name:    "v1",
+					Served:  true,
+					Storage: true,
+					Schema: &apiextensionsv1.CustomResourceValidation{
+						OpenAPIV3Schema: &apiextensionsv1.JSONSchemaProps{
+							XPreserveUnknownFields: utilpointer.BoolPtr(true),
+						},
+					},
+				},
+			},
+			Scope: apiextensionsv1.ClusterScoped,
+			Names: apiextensionsv1.CustomResourceDefinitionNames{
 				Plural: crdv1.VolumeSnapshotDataResourcePlural,
 				Kind:   reflect.TypeOf(crdv1.VolumeSnapshotData{}).Name(),
 			},
 		},
 	}
-	res, err := clientset.ApiextensionsV1beta1().CustomResourceDefinitions().Create(context.TODO(), crd, metav1.CreateOptions{})
+	res, err := clientset.ApiextensionsV1().CustomResourceDefinitions().Create(context.TODO(), crd, metav1.CreateOptions{})
 
 	if err != nil && !apierrors.IsAlreadyExists(err) {
 		glog.Fatalf("failed to create VolumeSnapshotDataResource: %#v, err: %#v",
 			res, err)
 	}
 
-	crd = &apiextensionsv1beta1.CustomResourceDefinition{
+	crd = &apiextensionsv1.CustomResourceDefinition{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: crdv1.VolumeSnapshotResourcePlural + "." + crdv1.GroupName,
 			Annotations: map[string]string{
 				"api-approved.kubernetes.io": "https://github.com/kubernetes-csi/external-snapshotter/pull/419",
 			},
 		},
-		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-			Group:   crdv1.GroupName,
-			Version: crdv1.SchemeGroupVersion.Version,
-			Scope:   apiextensionsv1beta1.NamespaceScoped,
-			Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
+		Spec: apiextensionsv1.CustomResourceDefinitionSpec{
+			Group: crdv1.GroupName,
+			//			Versions: crdv1.SchemeGroupVersion.Version,
+			Versions: []apiextensionsv1.CustomResourceDefinitionVersion{
+				{
+					Name:    "v1",
+					Served:  true,
+					Storage: true,
+					Schema: &apiextensionsv1.CustomResourceValidation{
+						OpenAPIV3Schema: &apiextensionsv1.JSONSchemaProps{
+							XPreserveUnknownFields: utilpointer.BoolPtr(true),
+						},
+					},
+				},
+			},
+			Scope: apiextensionsv1.NamespaceScoped,
+			Names: apiextensionsv1.CustomResourceDefinitionNames{
 				Plural: crdv1.VolumeSnapshotResourcePlural,
 				Kind:   reflect.TypeOf(crdv1.VolumeSnapshot{}).Name(),
 			},
 		},
 	}
-	res, err = clientset.ApiextensionsV1beta1().CustomResourceDefinitions().Create(context.TODO(), crd, metav1.CreateOptions{})
+	res, err = clientset.ApiextensionsV1().CustomResourceDefinitions().Create(context.TODO(), crd, metav1.CreateOptions{})
 	if err != nil && !apierrors.IsAlreadyExists(err) {
 		glog.Fatalf("failed to create VolumeSnapshotResource: %#v, err: %#v",
 			res, err)
 	}
+
+	glog.Infof("successfully created VolumeSnapshotResource")
 	return nil
 }
 
